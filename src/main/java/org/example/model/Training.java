c7018c20519722d0945686a5d66de0c70b5bb30e
anyone <anyone@gmail.com>
2025-10-30 22:01:54 +0500

    created all models

---
+++ src/main/java/org/example/model/Training.java
@@ -0,0 +1,69 @@
+package org.example.model;
+
+import java.time.LocalDate;
+
+public class Training {
+    private String traineeId;
+    private String trainerId;
+    private String trainingName;
+    private TrainingType trainingType;
+    private LocalDate trainingDate;
+    private Integer trainingDuration;
+
+    public Training(String traineeId, String trainerId, String trainingName, TrainingType trainingType, LocalDate trainingDate, int trainingDuration) {
+        this.traineeId = traineeId;
+        this.trainerId = trainerId;
+        this.trainingName = trainingName;
+        this.trainingType = trainingType;
+        this.trainingDate = trainingDate;
+        this.trainingDuration = trainingDuration;
+    }
+
+    public String getTraineeId() {
+        return traineeId;
+    }
+
+    public void setTraineeId(String traineeId) {
+        this.traineeId = traineeId;
+    }
+
+    public String getTrainerId() {
+        return trainerId;
+    }
+
+    public void setTrainerId(String trainerId) {
+        this.trainerId = trainerId;
+    }
+
+    public String getTrainingName() {
+        return trainingName;
+    }
+
+    public void setTrainingName(String trainingName) {
+        this.trainingName = trainingName;
+    }
+
+    public TrainingType getTrainingType() {
+        return trainingType;
+    }
+
+    public void setTrainingType(TrainingType trainingType) {
+        this.trainingType = trainingType;
+    }
+
+    public LocalDate getTrainingDate() {
+        return trainingDate;
+    }
+
+    public void setTrainingDate(LocalDate trainingDate) {
+        this.trainingDate = trainingDate;
+    }
+
+    public int getTrainingDuration() {
+        return trainingDuration;
+    }
+
+    public void setTrainingDuration(Integer trainingDuration) {
+        this.trainingDuration = trainingDuration;
+    }
+}
