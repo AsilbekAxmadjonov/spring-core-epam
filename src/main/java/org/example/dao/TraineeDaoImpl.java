c6c1c85917f7995d96cf7476b244a9e78fa59c8d
anyone <anyone@gmail.com>
2025-11-04 21:06:02 +0500

    removed casting and done with interfaces

---
+++ src/main/java/org/example/dao/TraineeDaoImpl.java
@@ -0,0 +1,62 @@
+package org.example.dao;
+
+import org.example.dao.TraineeDao;
+import org.example.model.Trainee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class TraineeDaoImpl implements TraineeDao {
+
+    private static final Logger logger = LoggerFactory.getLogger(TraineeDaoImpl.class);
+    private final Map<String, Trainee> traineeStorage;
+
+    public TraineeDaoImpl(@Qualifier("traineeStorage") Map<String, Trainee> traineeStorage) {
+        this.traineeStorage = traineeStorage;
+    }
+
+    @Override
+    public void save(Trainee trainee) {
+        traineeStorage.put(trainee.getUsername(), trainee);
+        logger.info("Saved new Trainee: {}", trainee.getUsername());
+    }
+
+    @Override
+    public void update(Trainee trainee) {
+        traineeStorage.put(trainee.getUsername(), trainee);
+        logger.info("Updated Trainee: {}", trainee.getUsername());
+    }
+
+    @Override
+    public void delete(Trainee trainee) {
+        if (trainee == null || trainee.getUsername() == null) {
+            logger.warn("Attempted to delete null Trainee or Trainee without username");
+            return;
+        }
+
+        if (traineeStorage.containsKey(trainee.getUsername())) {
+            traineeStorage.remove(trainee.getUsername());
+            logger.info("Deleted Trainee: {}", trainee.getUsername());
+        } else {
+            logger.warn("Attempted to delete non-existent Trainee: {}", trainee.getUsername());
+        }
+    }
+
+
+    @Override
+    public Trainee findByUsername(String username) {
+        logger.debug("Finding Trainee by username: {}", username);
+        return traineeStorage.get(username);
+    }
+
+    @Override
+    public List<Trainee> findAll() {
+        logger.info("Fetching all Trainees. Total count: {}", traineeStorage.size());
+        return traineeStorage.values().stream().toList();
+    }
+}
--- src/main/java/org/example/dao/TrainerDao.java
+++ src/main/java/org/example/dao/TrainerDao.java
@@ -1,44 +1,11 @@
 package org.example.dao;
 
 import org.example.model.Trainer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
-import java.util.Map;
 
-@Repository
-public class TrainerDao {
-
-    private static final Logger logger = LoggerFactory.getLogger(TrainerDao.class);
-    private final Map<String, Trainer> trainerStorage;
-
-    public TrainerDao(@Qualifier("trainerStorage") Map<String, Trainer> trainerStorage){
-        this.trainerStorage = trainerStorage;
-    }
-
-    public void save(Trainer trainer) {
-        trainerStorage.put(trainer.getUsername(), trainer);
-        logger.info("Saved Trainer: ", trainer.getUsername());
-    }
-
-    public void update(Trainer trainer) {
-        trainerStorage.put(trainer.getUsername(), trainer);
-        logger.info("Updated Trainer: ", trainer.getUsername());
-    }
-
-    public Trainer findByUsername(String username) {
-        logger.debug("Finding Trainer by username: ", username);
-        return (Trainer) trainerStorage.get(username);
-    }
-
-    public List<Trainer> findAll() {
-        logger.info("Fetching all Trainers. Total count of Trainers: ", trainerStorage.size());
-        return trainerStorage.values()
-                .stream()
-                .map(o -> (Trainer) o)
-                .toList();
-    }
+public interface TrainerDao {
+    void save(Trainer trainer);
+    void update(Trainer trainer);
+    Trainer findByUsername(String username);
+    List<Trainer> findAll();
 }
