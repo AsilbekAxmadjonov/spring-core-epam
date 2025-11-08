d25a30a9311a0db2089a62d6f85e874141ee2960
anyone <anyone@gmail.com>
2025-11-08 21:56:10 +0500

    small change

--- src/main/java/org/example/dao/TrainingDaoImpl.java
+++ src/main/java/org/example/dao/TrainingDaoImpl.java
@@ -1,8 +1,8 @@
 package org.example.dao;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.model.Training;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
@@ -10,30 +10,32 @@ import java.util.List;
 import java.util.Map;
 
 @Repository
+@Slf4j
 public class TrainingDaoImpl implements TrainingDao {
 
-    private static final Logger logger = LoggerFactory.getLogger(TrainingDaoImpl.class);
-    private final Map<String, Training> trainingStorage;
+    private Map<String, Training> trainingStorage;
 
-    public TrainingDaoImpl(@Qualifier("trainingStorage") Map<String, Training> trainingStorage) {
+    // Setter-based injection for trainingStorage
+    @Autowired
+    public void setTrainingStorage(@Qualifier("trainingStorage") Map<String, Training> trainingStorage) {
         this.trainingStorage = trainingStorage;
     }
 
     @Override
     public void save(Training training) {
-        logger.info("Saved new Training: {}", training.getTrainingName());
         trainingStorage.put(training.getTrainingName(), training);
+        log.info("Saved new Training: {}", training.getTrainingName());
     }
 
     @Override
     public Training findByName(String name) {
-        logger.debug("Finding Training by name: {}", name);
+        log.debug("Finding Training by name: {}", name);
         return trainingStorage.get(name);
     }
 
     @Override
     public List<Training> findAll() {
-        logger.info("Fetching all Trainings. Total count: {}", trainingStorage.size());
+        log.info("Fetching all Trainings. Total count: {}", trainingStorage.size());
         return trainingStorage.values().stream().toList();
     }
 }
