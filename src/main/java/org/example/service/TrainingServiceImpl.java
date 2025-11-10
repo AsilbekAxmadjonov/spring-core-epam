cfba6593069797d84f63525168b2a0055312a788
anyone <anyone@gmail.com>
2025-11-10 19:11:47 +0500

    changed a little

--- src/main/java/org/example/service/TrainingServiceImpl.java
+++ src/main/java/org/example/service/TrainingServiceImpl.java
@@ -1,18 +1,17 @@
+
 package org.example.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.dao.TrainingDao;
 import org.example.model.Training;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class TrainingServiceImpl implements TrainingService {
-
-    private static final Logger logger = LoggerFactory.getLogger(TrainingServiceImpl.class);
 
     private TrainingDao trainingDao; // cannot be final with setter injection
 
@@ -24,19 +23,19 @@ public class TrainingServiceImpl implements TrainingService {
 
     @Override
     public void createTraining(Training training) {
-        logger.info("Creating new Training: {}", training.getTrainingName());
+        log.info("Creating new Training: {}", training.getTrainingName());
         trainingDao.save(training);
     }
 
     @Override
     public Training getTraining(String name) {
-        logger.debug("Getting Training by name: {}", name);
+        log.debug("Getting Training by name: {}", name);
         return trainingDao.findByName(name);
     }
 
     @Override
     public List<Training> listAll() {
-        logger.info("Listing all Trainings");
+        log.info("Listing all Trainings");
         return trainingDao.findAll();
     }
 }
