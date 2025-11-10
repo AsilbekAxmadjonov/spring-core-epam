95e7887004a49cb7d4f312b1128c8e83bb98bd3f
anyone <anyone@gmail.com>
2025-11-10 14:53:48 +0500

    modified

--- src/main/java/org/example/dao/TraineeDaoImpl.java
+++ src/main/java/org/example/dao/TraineeDaoImpl.java
@@ -2,6 +2,7 @@ package org.example.dao;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.model.Trainee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
