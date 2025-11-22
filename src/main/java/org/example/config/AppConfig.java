7eb5f09aaa782c8e2a60d3bead5ad64bacf5d533
Asilbek Axmadjonov <147590514+AsilbekAxmadjonov@users.noreply.github.com>
2025-11-23 01:40:40 +0500

    Feature/docker compose (#12)
    * run with docker compose
    
    * Reverted DataSourceConfig and application.yaml — keeping only docker-compose changes

--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -1,14 +1,18 @@
 package org.example.config;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
 @PropertySource(value = "classpath:application.yaml", factory = YamlPropertySourceFactory.class)
+@EnableTransactionManagement
 public class AppConfig {
 
     // Enables Spring to read property placeholders like ${data.trainers.path}
