dd482156afc729f1b5c709941b84f363e735e4fe
AsilbekAxmadjonov <a.axmadjonov@newuu.uz>
2025-11-26 14:36:23 +0500

    added @Modified annotation to return correct type

--- src/main/java/org/example/repository/TraineeRepo.java
+++ src/main/java/org/example/repository/TraineeRepo.java
@@ -2,6 +2,7 @@ package org.example.repository;
 
 import org.example.entity.Trainee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,6 +13,7 @@ public interface TraineeRepo extends JpaRepository<Trainee, Long> {
     @Query("SELECT t FROM Trainee t WHERE t.user.username = :username")
     Optional<Trainee> findByUsername(@Param("username") String username);
 
+    @Modifying
     @Query("DELETE FROM Trainee t WHERE t.user.username = :username")
     void deleteByUsername(@Param("username") String username);
 }
